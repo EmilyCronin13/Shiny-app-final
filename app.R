4cda424aa9631dfc9e744469a09bef248ddd2608
zainab-ali-9 <zainabsiddig.ali@gmail.com>
2024-12-22 19:05:29 +0000

    adding this part by zainab

--- app.R
+++ app.R
@@ -37,3 +37,21 @@ ui <- navbarPage(
                    heading_font = font_google("Raleway")),
   
   
+  #Adding multiple panels to the shiny page : https://www.youtube.com/watch?v=V8ne6_F7lIM
+  
+  
+  
+  #Introduction Tab to the DIG trial
+  
+  tabPanel("Introduction to the DIG Trial", fluidPage(tags$head( tags$style(HTML("
+
+.about-page {background-image: url('https://www.ucsfhealth.org/-/media/project/ucsf/ucsf-health/medical-tests/hero/digoxin-test-2x.jpg?rev=ae7227c07f7e4a338f1df1628fbcee89');background-size: cover;
+
+background-position: center;
+
+text-align: center;
+
+color: white;
+
+padding: 50px;}
+
